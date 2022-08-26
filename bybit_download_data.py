3cafdeeadc8aec370eba00763d09f0bf5a4c6b27
paulnheera <paul.nheera@gmail.com>
2022-08-26 12:12:02 +0200

    Update to bybit_download_data. Allow for end_time to be none.

--- bybit_download_data.py
+++ bybit_download_data.py
@@ -15,7 +15,7 @@ from pybit import usdt_perpetual
 # Connect to API
 client = usdt_perpetual.HTTP(endpoint="https://api-testnet.bybit.com")
 
-def get_bybit_data(symbol, interval, start_time, end_time):
+def get_bybit_data(symbol, interval, start_time, end_time=None):
     
     ''' Download data from exchange 
     
@@ -31,54 +31,68 @@ def get_bybit_data(symbol, interval, start_time, end_time):
         End point of data
     '''
     
-    # Convert time variables to timestamps
-    
     # Check that time variables are string
     
+    # Convert time variables to timestamps
     start_time = datetime.strptime(start_time, '%Y-%m-%d %H:%M')
-    end_time = datetime.strptime(end_time, '%Y-%m-%d %H:%M')
-    
     start_ts = int(start_time.replace(tzinfo=timezone.utc).timestamp())
-    end_ts = int(end_time.replace(tzinfo=timezone.utc).timestamp())
+    
+    if end_time is not None:
+        end_time = datetime.strptime(end_time, '%Y-%m-%d %H:%M')
+        end_ts = int(end_time.replace(tzinfo=timezone.utc).timestamp())
     
     # set main list object & current time cursor
     raw_ls = []
     c_ts = start_ts
     
-    # while loop to download several pages of data
-    while c_ts < end_ts:
+    if end_time is None:
         
-        # time difference
-        diff = int( (end_ts - c_ts) / (interval*60) )    # divide by 60 to get to mins, divide by interval to get to frequency
-        
-        print("Downloading data from {}".format(pd.to_datetime(c_ts, unit='s')))
-        print("{} bars remaining...".format(diff))
-        
-        print("-"*55)
-        
-        # download data
-        temp = client.query_kline(
+        raw_ls = client.query_kline(
                 symbol=symbol,
                 interval=interval,
-                limit=min(200,diff),
-                from_time=c_ts
+                from_time=start_ts
         )
-        
-        # append list to main list
-        temp = temp['result']
-        raw_ls.extend(temp)
     
-        # update time cursor
-        c_ts = raw_ls[-1]['start_at'] + interval*60  # interval (m) 
-        
-        # sleep for a bit
-        time.sleep(1)
-        
-        
+        raw_ls = raw_ls['result']
     
+    else:
+    
+        # while loop to download several pages of data
+        while c_ts < end_ts:
+            
+            # time difference
+            diff = int( (end_ts - c_ts) / (interval*60) )    # divide by 60 to get to mins, divide by interval to get to frequency
+            
+            print("Downloading data from {}".format(pd.to_datetime(c_ts, unit='s')))
+            print("{} bars remaining...".format(diff))
+            
+            print("-"*55)
+            
+            # download data
+            temp = client.query_kline(
+                    symbol=symbol,
+                    interval=interval,
+                    limit=min(200,diff),
+                    from_time=c_ts
+            )
+            
+            # append list to main list
+            temp = temp['result']
+            raw_ls.extend(temp)
+        
+            # update time cursor
+            c_ts = raw_ls[-1]['start_at'] + interval*60  # interval (m)
+            
+            ##IMPROVE: CREATE AN EXCEPTION FOR WHEN c_ts IS GREATER THAN THE AVAILABLE TIME.
+            ## I.E. WHERE IT CANNOT BE USED AS VALUE FOR THE ARGUMENT from_time.
+            
+            # sleep for a bit
+            time.sleep(1)
+        
+
     # convert to dataframe
     df = pd.DataFrame(raw_ls)
-    
+        
     # rename colums
     df.rename(columns = {'symbol':'Symbol',
                            'period':'Period',
@@ -94,7 +108,7 @@ def get_bybit_data(symbol, interval, start_time, end_time):
                           }, inplace=True)
     
     # Convert time column to datetime
-    df['Start_at'] = pd.to_datetime(df['Start_at'], unit='s')
+    #df['Start_at'] = pd.to_datetime(df['Start_at'], unit='s')
     df['Time'] = pd.to_datetime(df['Time'], unit='s')
 
     # Convert fields to numerical values
