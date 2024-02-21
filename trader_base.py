41542c83386ca577a0a0a1e1aaff5b7ca67dd2e2
paulnheera <paul.nheera@gmail.com>
2024-02-21 21:17:57 +0200

    Update bybit API integration for order placement methods to align with latest bybit API version.

--- trader_base.py
+++ trader_base.py
@@ -3,6 +3,7 @@
 # Import Libraries
 import pandas as pd
 import numpy as np
+import math
 from math import floor
 import pickle
 import time
@@ -32,13 +33,13 @@ class TraderBase(object):
     =======
     '''
     
-    def __init__(self, conf_file, exchange, symbol, interval, leverage=1, sl=None, tp=None, verbose=True):
+    def __init__(self, conf_file, exchange, symbol, interval, leverage=None, sl=None, tp=None, verbose=True):
         
         self.conf_file =conf_file
         self.exchange = exchange
         self.symbol = symbol
         self.interval = interval
-        self.leverage = leverage
+        self.leverage = 1
         self.sl = sl
         self.tp = tp
         #self.initial_amount = amount
@@ -76,6 +77,9 @@ class TraderBase(object):
         else:
             print("Exchange not currently supported!")
             
+        # Get symbol properties    
+        self.symbol_properties = self.get_symbol_properties()
+            
         # Set the leverage
         if self.leverage is not None:
             try:
@@ -86,6 +90,19 @@ class TraderBase(object):
                 
             except:
                 print('Leverage not modified.')
+                
+    def get_symbol_properties(self):
+        res = self.client.get_instruments_info(category='linear', symbol=self.symbol)
+        result = res['result']['list'][0]
+        
+        tick_size = float(result['priceFilter']['tickSize'])
+        price_prec = int(-math.log10(tick_size))
+        
+        qty_step = float(result['lotSizeFilter']['qtyStep'])
+        qty_prec = int(-math.log10(qty_step))
+        
+        return {'Tick Size':tick_size, 'Price Precision':price_prec, 
+                'Qty Step':qty_step, 'Quantity Precision':qty_prec}
             
     def get_data(self, min_length):
         '''Retrieves and prepares the latest data
@@ -124,19 +141,18 @@ class TraderBase(object):
         Time, Bid, Ask
 
         '''
-        quote = self.client.latest_information_for_symbol(symbol = self.symbol)
-        time = pd.to_datetime(quote['time_now'], unit='s')
-        ask_price = float(quote['result'][0]['ask_price'])
-        bid_price = float(quote['result'][0]['bid_price'])
+        res = self.client.get_tickers(category='linear', symbol = self.symbol)
+        time = pd.to_datetime(res['time'], unit='ms')
+        ask_price = float(res['result']['list'][0]['ask1Price'])
+        bid_price = float(res['result']['list'][0]['bid1Price'])
         
         # return tuple/ dataframe/ dictionary
         return {'time':time, 'ask_price':ask_price, 'bid_price':bid_price}
     
-    def get_available_balance(self):
-        res = self.client.get_wallet_balance(coin="USDT")['result']
-        avail_bal = res['USDT']['available_balance'] 
-        
-        return avail_bal
+    def get_available_balance(self, coin='USDT', accountType='UNIFIED'):
+        res = self.client.get_wallet_balance(accountType=accountType, coin=coin)
+        balance = float(res['result']['list'][0]['coin'][0]['walletBalance']) 
+        return balance
         
     def print_balance(self):
         '''Print out current available balance info.
@@ -146,11 +162,9 @@ class TraderBase(object):
         None.
 
         '''
-        
         res = self.client.get_wallet_balance(coin="USDT")['result']
         avail_bal = res['USDT']['available_balance']  
-        
-        print(f'{date} | current balance {avail_bal:.2f}')
+        #print(f'{date} | current balance {avail_bal:.2f}')
         
     def print_net_wealth(self):
         '''Print out current net wealth
@@ -163,52 +177,57 @@ class TraderBase(object):
         '''
         res = self.client.get_wallet_balance(coin="USDT")['result']
         net_wealth = res['USDT']['equity']
-        
-        print(f'{date} | current net wealth {net_wealth:.2f}')
+        #print(f'{date} | current net wealth {net_wealth:.2f}')
     
     def place_buy_order(self, units=None, amount=None, sl=None, tp=None):
-        # get quote
+        # Get quote
         quote = self.get_quote()
         time = quote['time']
         ask_price = quote['ask_price']
         
-        # get available balance
-        res = self.client.get_wallet_balance(coin="USDT")['result']
-        amount = res['USDT']['available_balance']  
+        # Get available balance (TODO: only if units is not specified.)
+        amount = self.get_available_balance() 
         
-        # set stop prices
+        # Symbol Properties
+        tick_size = self.symbol_properties['Tick Size']
+        price_prec = self.symbol_properties['Price Precision']
+        qty_step = self.symbol_properties['Qty Step']
+        qty_prec = self.symbol_properties['Quantity Precision']
+        
+        # Set stop prices
         if tp is not None:
-            take_profit = round(ask_price * (1 + tp), 3)
+            take_profit = round(ask_price * (1 + tp) / tick_size) * tick_size
         else:
             take_profit = None
         if sl is not None:
-            stop_loss = round(ask_price * (1 - sl), 3)
+            stop_loss = round(ask_price * (1 - sl) / tick_size) * tick_size
         else: stop_loss = None
         
-        # caculate qty (using fees) (if units is none)
+        # Calculate qty (using fees) (if units is none)
         if units is None:
-            units = (amount * self.leverage)/ (ask_price * (0.0012 * self.leverage + 0.9994))
-            units = floor(units*100)/100
+            units = (amount * self.leverage) / (ask_price * (0.0012 * self.leverage + 0.9994))
+            units = floor(units * qty_step) / qty_step
         
-        # place order
+        # Place order
         order = self.client.place_active_order(
-            symbol = self.symbol,
-            side = "Buy",
-            order_type = "Market",
-            qty= units,                              # units = qty
-            #price=8083,
-            take_profit = take_profit,
-            stop_loss = stop_loss,
-            time_in_force = "GoodTillCancel",
-            reduce_only = False,
-            close_on_trigger = False,
-            position_idx = 0            
-        )
+                    category='linear',
+                    symbol = self.symbol,
+                    side = "Buy",
+                    orderType = "Market",
+                    qty= f'{units:.{qty_prec}f}',                              # units = qty
+                    #price=8083,
+                    take_profit = f'{take_profit:.{price_prec}f}' ,
+                    stop_loss = f'{stop_loss:.{price_prec}f}',
+                    timeInForce = "PostOnly",                                  # WHAT IS POST ONLY
+                    orderLinkId = f"{self.symbol} - {datetime.now()}",
+                    isLeverage = 0,
+                    orderFilter = "Order"       
+                )
         
         # Update order history
-        self.order_history.append(order['result'])
+        self.order_history.append(order['result']) ### TODO: Check out response.
         
-        # print
+        # Print
         if self.verbose:
             print(f'{time:%Y-%m-%d %H:%M:%S} | buying {units} units at {ask_price:.2f}')
             #self.print_balance()   ## *** Still need to fix these functions ***
@@ -216,46 +235,50 @@ class TraderBase(object):
         
         
     def place_sell_order(self, units=None, amount=None, sl=None, tp=None):
-        # get quote
+        # Get quote
         quote = self.get_quote()
         time = quote['time']
         bid_price = quote['bid_price']
         
-        # get available balance
-        # if amount == all
-        res = self.client.get_wallet_balance(coin="USDT")['result']
-        amount = res['USDT']['available_balance'] 
+        # Get available balance (TODO: only if units is not specified.)
+        amount = self.get_available_balance()
         
-        # set stop prices
+        # Symbol Properties
+        tick_size = self.symbol_properties['Tick Size']
+        price_prec = self.symbol_properties['Price Precision']
+        qty_step = self.symbol_properties['Qty Step']
+        qty_prec = self.symbol_properties['Quantity Precision']
+        
+         # Set stop prices
         if tp is not None:
-            take_profit = round(bid_price * (1 - tp), 3)
+            take_profit = round(bid_price * (1 - tp) / tick_size) * tick_size
         else:
             take_profit = None
         if sl is not None:
-            stop_loss = round(bid_price * (1 + sl), 3)
-        else:
-            stop_loss = None
+            stop_loss = round(bid_price * (1 + sl) / tick_size) * tick_size
+        else: stop_loss = None
 
         # caculate qty (using fees) (if units is none)
         if units is None:
             units = (amount * self.leverage)/ (bid_price * (0.0012 * self.leverage + 1.0006))
-            units = floor(units*100)/100
+            units = floor(units * qty_step) / qty_step
         
         # place order
         order = self.client.place_active_order(
-            symbol = self.symbol,
-            side = "Sell",
-            order_type = "Market",
-            qty= units,                              # units = qty
-            #price=8083,
-            take_profit = take_profit,
-            stop_loss = stop_loss,
-            time_in_force = "GoodTillCancel",
-            reduce_only = False,
-            close_on_trigger = False,
-            position_idx = 0            
-        )
-        
+                    category='linear',
+                    symbol = self.symbol,
+                    side = "Sell",
+                    orderType = "Market",
+                    qty= f'{units:.{qty_prec}f}',                              # units = qty
+                    #price=8083,
+                    take_profit = f'{take_profit:.{price_prec}f}' ,
+                    stop_loss = f'{stop_loss:.{price_prec}f}',
+                    timeInForce = "PostOnly",                                  # WHAT IS POST ONLY
+                    orderLinkId = f"{self.symbol} - {datetime.now()}",
+                    isLeverage = 0,
+                    orderFilter = "Order"       
+                )
+                
         # Update order history
         self.order_history.append(order['result'])
         
@@ -331,10 +354,11 @@ class TraderBase(object):
     
     
 if __name__ == '__main__':
-    tb = TraderBase(conf_file='algo_trading.cfg',
+    trader = TraderBase(conf_file='algo_trading.cfg',
                     exchange='bybit',
-                    symbol='ETHUSDT',
+                    symbol='ALICEUSDT',
                     interval=60)
     
-    tb.get_data(100)
+    # tb.get_data(100)
+    trader.get_quote()
 
